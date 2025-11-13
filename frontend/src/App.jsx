2c13367284203962ce3ed24c2c87d782ad6e9f37
root <root@MeadXPS.localdomain>
2025-11-13 18:01:19 -0500

    Initial Job creation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import JobList from './JobList'
 
 function App() {
   
   //Define state that stores jobs
   const [jobs, setJobs] = useState([])
 
-  //
+  //Calls fetchJobs() whenever the page renders
   useEffect(() => {
     fetchJobs()
   }, [])
 
-  //
+  //Send request to backend to receive list of jobs
   const fetchJobs = async () => {
     const response = await fetch("http://127.0.0.1:5000/jobs")
     const data = await response.json()
@@ -21,11 +22,7 @@ function App() {
     console.log(data.jobs)
   }
 
-  return (
-    <>
-      
-    </>
-  )
+  return <JobList jobs={jobs} />
 }
 
 export default App
---
+++ frontend/src/JobList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const JobList = ({jobs}) =>{
+    return <div>
+        <h2>Job List</h2>
+        <table>
+            <thead>
+                <tr>
+                    <button>Create New Job</button>
+                </tr>
+            </thead>
+            <tbody>
+                {jobs.map((job) => (
+                    <tr key={job.jobid}>
+                        <td>{job.username}</td>
+                        <td>{job.address}</td>
+                        <td>{job.description}</td>
+                        <td>{job.fixerName}</td>
+                        <td>{job.status}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+
+}
+
+export default JobList
