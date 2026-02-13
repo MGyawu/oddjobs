697ed66ccc035c2c1e1d1af50a50a96f56ea5cd3
root <root@MeadXPS.localdomain>
2026-02-12 21:45:22 -0500

    First set of revamped tests

--- backend2/tests/conftest.py
+++ backend2/tests/conftest.py
@@ -18,3 +18,50 @@ def app():
 def client(app):
     return app.test_client()
 
+#Sample test data for the tests to test app routes/functions
+@pytest.fixture
+def test_user_data():
+    return [
+        {
+            "username": "JohnUserName",
+            "password": "Secret123",
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "John.Doe@gmail.com"
+        },
+        {
+            "username": "ADiffName",
+            "lastName": "Doe",
+            "email": "ADiffName@gmail.com"
+        }
+    ]
+
+@pytest.fixture
+def test_job_data():
+    return [
+        {
+            #"jobid" : "67",
+            "username" : "JohnUserName",
+            "address" : "Duff City",
+            "description" : "Mow my lawn gang",
+            "fixerName" : "Fix it felix",
+        },
+        {
+            #"jobid" : "67",
+            "username" : "JohnUserName",
+            "address" : "Duff City",
+            "description" : "paint my house",
+            "fixerName" : "Wreck it Ralph",
+        },
+        {
+            #"jobid" : "67",
+            "username" : "JaneUserName",
+            "address" : "Duck City",
+            "description" : "Build my shelf",
+            "fixerName" : "Fix it felix",
+        },
+        {
+            "username": "JohnUserName",
+            "fixerName": "Fix it felix",
+        }
+    ]
